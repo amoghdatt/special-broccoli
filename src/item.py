1c5eca40b4695107c56ead538cadd500b58b7a1a
Amogh <amoghdatt@gmail.com>
2022-07-29 19:30:36 +0530

    [ALE] add item class to manage items

---
+++ src/item.py
@@ -0,0 +1,9 @@
+
+
+class Item:
+
+    def __init__(self, name, is_imported, price, category) -> None:
+        self.name = name
+        self.is_imported = is_imported
+        self.price = price
+        self.category = category
